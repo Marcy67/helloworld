3f8f36e613f17e59d7aec0e78a0b5e400aabe527
Marcy Crain <MCrain4501@student.eggc.edu>
2020-10-07 13:55:43 -0400

    modified hello messages

--- hello.py
+++ hello.py
@@ -3,4 +3,7 @@ def hello(msg):
     #works only on Python 3.7+
     print(f"hello {msg}!")
 
-if__name__=="__main__":
+if __name__=="__main__":
+    hello("world")
+    hello("Alice")
+    hello("Bob")
