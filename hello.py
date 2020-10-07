926a7e7e55cf7ef83cafa5d1a53aa7ad4449d84c
Marcy Crain <MCrain4501@student.eggc.edu>
2020-10-07 13:44:03 -0400

    added hello.py

--- hello.py
+++ hello.py
@@ -1,7 +1,6 @@
 #first phython program
 def hello(msg):
-    #works only on Phython 3.7+
-    print(f"hello {msg}")
+    #works only on Python 3.7+
+    print(f"hello {msg}!")
 
 if__name__=="__main__":
-  hello("world")
